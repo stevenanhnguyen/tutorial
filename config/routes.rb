4cd733c9249f86c33b90a7d1d0efc8a194e11be1
stevenanhnguyen <at.mrsteven@gmail.com>
2018-01-15 13:49:10 +0700

    Revert "Modeling users"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
